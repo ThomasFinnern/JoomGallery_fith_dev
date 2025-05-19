5adddfe97995b502f5365eb36ed8d1774582a0cf
ThomasFinnern <ThomasFinnernE5570@users.noreply.github.com>
2025-05-19 22:57:05 +0200

    jg_assignUser2Group.bat <username> <usergroup>

--- cli/jg_assignUser2Group.bat
+++ cli/jg_assignUser2Group.bat
@@ -1,59 +1,34 @@
-
-
-ToDo: JG, JG2,...
-Example 
-php cli/joomla.php user:addtogroup \
-  --username="gewünschterBenutzer" \
-  --group="Super Users"
-
-
-
-
-
-
-
-
-
-
-
 @ECHO OFF
-REM create some users for joomgallery  
-
+REM assign usergroup '' to user   
+REM call jg_assignUser2Group.bat <username> <usergroup>
 REM needs to be startet in CLI folder. or use cli\joomla.php below
 
 CLS
 
-php joomla.php user:list
-REM pause
+REM if A%password%==A 
+if  "%~1"=="" (
+	ECHO The user name is needed as first argument
+	ECHO Please add the user name as first argument 
+	GOTO :EOF
+)
+set username=%1
 
-REM   joomgallery:categories:list [options]
-REM 
-REM Options:
-REM       --owner[=OWNER]          username (created_by)
-REM   -s, --search[=SEARCH]        COM_JOOMGALLERY_CLI_CONFIG_SEARCH
-REM   -h, --help                   Display the help information
-REM   -q, --quiet                  Flag indicating that all output should be silenced
-REM   -V, --version                Displays the application version
-REM       --ansi                   Force ANSI output
-REM       --no-ansi                Disable ANSI output
-REM   -n, --no-interaction         Flag to disable interacting with the user
-REM       --live-site[=LIVE-SITE]  The URL to your site, e.g. https://www.example.com
-REM   -v|vv|vvv, --verbose         Increase the verbosity of messages: 1 for normal output, 2 for more verbose output and 3 for debug
-REM 
-REM Help:
-REM   joomgallery:categories:list will list all joomgallery categories
-REM 
-REM   Usage: php joomla.php joomgallery:categories:list
-REM set /p password="Please enter password: "
 
-php joomla.php user:add --username="jg" --name="jg manage+category" --email="jg@tomfinnern.de" --usergroup="JG_user_upload" --password="%password%"
-php joomla.php user:add --username="jg2" --name="jg2 manage+category" --email="jg2@tomfinnern.de" --usergroup="JG_user_upload" --password="%password%"
-php joomla.php user:add --username="jg3" --name="jg3 manage+category" --email="jg3@tomfinnern.de" --usergroup="JG_user_upload" --password="%password%"
-php joomla.php user:add --username="jg4" --name="jg4 manage+category" --email="jg4@tomfinnern.de" --usergroup="JG_user_upload" --password="%password%"
+set usergroup=JG_user_upload
 
-php joomla.php user:add --username="JGnoManage" --name="jg no manage" --email="jgNoManage@tomfinnern.de" --usergroup="JG_user_upload" --password="%password%"
+REM if NOT "%~2"=="" (
+if "%~2"=="" (
+	ECHO The user group may be the second argument
+	ECHO Using standard user group "JG_user_upload"
+)
 
-REM ToDo: at home : add no cat ... users 
+if NOT "%~2"=="" (
+	set usergroup=%2
+)
+
+ECHO php joomla.php user:addtogroup --username="%username%" --group="%usergroup%"
+php joomla.php user:addtogroup --username="%username%" --group="%usergroup%"
+
 
 php joomla.php user:list
 pause
