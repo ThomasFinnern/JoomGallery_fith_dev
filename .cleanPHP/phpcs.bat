a2595a132f4c1f051d962633d19f565079ce8833
ThomasFinnern <ThomasFinnern@users.noreply.github.com>
2025-11-03 11:57:28 +0100

    Added batches for phpstan/phpcs/phpcbx calls

---
+++ .cleanPHP/phpcs.bat
@@ -0,0 +1,8 @@
+@ECHO OFF
+REM ./libraries/vendor/bin/phpcs --extensions=php -p --standard=ruleset.xml 
+REM d:\Entwickl\2025\_gitHub\JoomGallery_fith\ruleset.xml 
+
+ECHO "C:\Users\finnern\AppData\Roaming\Composer\vendor\squizlabs\php_codesniffer\bin\phpcs.bat" --extensions=php -p --standard=d:\Entwickl\2025\_gitHub\JoomGallery_fith\ruleset.xml d:\Entwickl\2025\_gitHub\JoomGallery_fith_feature_user_panel_uploadd:\Entwickl\2025\_gitHub\JoomGallery_fith_feature_user_panel_upload
+"C:\Users\finnern\AppData\Roaming\Composer\vendor\squizlabs\php_codesniffer\bin\phpcs.bat" --extensions=php -p --standard=d:\Entwickl\2025\_gitHub\JoomGallery_fith\ruleset.xml d:\Entwickl\2025\_gitHub\JoomGallery_fith_feature_user_panel_upload
+REM "C:\Users\finnern\AppData\Roaming\Composer\vendor\squizlabs\php_codesniffer\bin\phpcs.bat" --help
+
